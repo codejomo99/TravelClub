dc0a821bfbe150ba69237c5c4ff7c9cc34b46a44
hlep <hlep@naver.com>
2024-01-29 16:54:36 +0900

    컬렉션을 이용한 구현실습(2)

--- src/travelClub/store/logic/ClubMapStore.java
+++ src/travelClub/store/logic/ClubMapStore.java
@@ -1,38 +1,61 @@
 package travelClub.store.logic;
-
 import travelClub.entity.TravelClub;
 import travelClub.store.ClubStore;
 
-import java.util.List;
+import java.util.*;
 
 public class ClubMapStore implements ClubStore {
+
+    private Map<String, TravelClub> clubMap;
+    public ClubMapStore(){
+        this.clubMap = new HashMap<>();
+    }
+
+
     @Override
     public void create(TravelClub travelClub) {
-
+        this.clubMap.put(travelClub.getId(),travelClub);
     }
 
     @Override
     public TravelClub retrive(String clubId) {
-        return null;
+
+        return this.clubMap.get(clubId);
     }
 
     @Override
     public List<TravelClub> retireveAllByName(String clubName) {
-        return null;
+        List<TravelClub> foundClubs = new ArrayList<>();
+
+        for(TravelClub club : this.clubMap.values()){
+            if(club.getClubName().equals(clubName)){
+                foundClubs.add(club);
+            }
+        }
+
+//        Iterator<TravelClub> iterator = this.clubMap.values().iterator();
+//        while ((iterator.hasNext())){
+//            TravelClub club = iterator.next();
+//            if(club.getClubName().equals(clubName)){
+//                foundClubs.add(club);
+//            }
+//        }
+
+        return foundClubs;
     }
 
     @Override
     public List<TravelClub> retrieveAll() {
-        return null;
+        return new ArrayList<>(this.clubMap.values());
     }
 
     @Override
-    public void update(TravelClub updateClib) {
-
+    public void update(TravelClub updateClub) {
+        this.clubMap.put(updateClub.getId(), updateClub);
     }
 
     @Override
     public void delete(String clubId) {
-
+        this.clubMap.remove(clubId);
     }
 }
